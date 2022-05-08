4ddc687ad280341d70676d592ecd3d57dbb2852a
Lily <lilyfields11@bigpond.com>
2022-05-09 07:46:01 +1000

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const React =require('react');
+const ReactDOM =reuire ('react-dom');
+const App =require('./components/App');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
